f7907f61c5cb7aba2f187b0a47d2152b1fba72c5
明扬 <my@famer.me>
2025-03-21 16:05:11 +0800

    update answer to 2025s

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -51,17 +51,43 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
-pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
+/// 获取任务信息
+/// 在 ch3 中，我们的系统已经能够支持多个任务分时轮流运行，我们希望引入一个新的系统调用 ``sys_trace``（ID 为 410）用来追踪当前任务系统调用的历史信息，并做对应的修改。定义如下。
+///
+/// fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize
+/// 调用规范：
+/// 这个系统调用有三种功能，根据 trace_request 的值不同，执行不同的操作：
+///
+/// 如果 trace_request 为 0，则 id 应被视作 *const u8 ，表示读取当前任务 id 地址处一个字节的无符号整数值。此时应忽略 data 参数。返回值为 id 地址处的值。
+///
+/// 如果 trace_request 为 1，则 id 应被视作 *const u8 ，表示写入 data （作为 u8，即只考虑最低位的一个字节）到该用户程序 id 地址处。返回值应为0。
+///
+/// 如果 trace_request 为 2，表示查询当前任务调用编号为 id 的系统调用的次数，返回值为这个调用次数。本次调用也计入统计 。
+///
+/// 否则，忽略其他参数，返回值为 -1。
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
     trace!("kernel: sys_task_info");
-    let task = TASK_MANAGER.current_task();
-    let now = get_time_ms();
 
-    let info = task.info();
+    match trace_request {
+        0 => {
+            let data_ptr = id as *const u8;
+            unsafe { *data_ptr as isize }
+        }
+        1 => {
+            let data_ptr = id as *mut u8;
+            unsafe { *data_ptr = data as u8 };
+            0
+        }
+        2 => {
+            let task = TASK_MANAGER.current_task();
 
-    unsafe {
-        (*ti).status = task.status();
-        (*ti).time = now - info.start_time;
-        (*ti).syscall_times.copy_from_slice(&info.syscall_times);
+            let info = task.info();
+
+            info.syscall_times[id] as isize
+        }
+        _ => {
+            trace!("Unsupported trace request: {}", trace_request);
+            -1
+        }
     }
-    0
 }
